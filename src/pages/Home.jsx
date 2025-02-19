15cdd2daafdea0de285c5c357367acd6208295b9
bahrom <baxrom@email.com>
2025-02-19 10:19:16 +0500

    correct Sidebar import path in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import InvoiceDashboard from "../components/InvoiceDashboard";
-import Sidebar from "../components/sidebar";
+import Sidebar from "../components/Sidebar";
 
 function Home() {
   return (
